622ea7040c4fcbd67e285e3b7956c52783c955c3
Alvin <huerox@msn.com>
2022-02-14 20:45:25 -0800

    initialized user and car objects and implemented CRUD operations using the ApplicationContextProvider.

--- spring-boot-demo/src/main/java/com/revature/springbootdemo/SpringBootDemoApplication.java
+++ spring-boot-demo/src/main/java/com/revature/springbootdemo/SpringBootDemoApplication.java
@@ -1,7 +1,13 @@
 package com.revature.springbootdemo;
 
+import com.revature.springbootdemo.beans.Repositories.CarRepository;
+import com.revature.springbootdemo.beans.Repositories.UserRepository;
+import com.revature.springbootdemo.beans.models.Cars;
+import com.revature.springbootdemo.beans.models.Users;
+import com.revature.springbootdemo.beans.utilities.ApplicationContextProvider;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication(scanBasePackages = "com.revature.springbootdemo.beans")
 public class SpringBootDemoApplication {
@@ -10,6 +16,19 @@ public class SpringBootDemoApplication {
 
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 
+		ApplicationContext context = ApplicationContextProvider.getApplicationContext();
+		CarRepository carRepository = context.getBean(CarRepository.class);
+		UserRepository userRepository = context.getBean(UserRepository.class);
+
+		// TODO Instantiate instance object and run CRUD on these objects
+		Users user = new Users();
+		Cars car = new Cars();
+
+		user.setFirstName("Joseph");
+		userRepository.save(user);
+
+		car.setMake("Dodge");
+		carRepository.save(car);
 	}
 
 }
